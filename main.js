abdfd275a3e75dc57e93893d752cba81aad17f3e
Garmhausen <robertgarmhausen@gmail.com>
2018-03-23 14:46:09 -0400

    Add toggle option to test for 200 digits.

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 'use strict';
 
-var pi = '3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'; // 100 digits worth
+var piOne = '3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679'; // 100 digits worth
+
+var piTwo = '3.14159265358979323846264338327950288419716939937510582097494459230781640628620899862803482534211706798214808651328230664709384460955058223172535940812848111745028410270193852110555964462294895493038196';  // 200 digits worth
+
 
 var inputElem = document.querySelector('.input-wrapper');
 var guessElem = document.querySelector('textarea');
 var buttonElem = document.querySelector('button');
 var answerElem = document.querySelector('#answer');
+var selectElem = document.querySelector('select');
 
 buttonElem.addEventListener('click', function() {
   var guess = guessElem.value;
@@ -21,5 +25,14 @@ buttonElem.addEventListener('click', function() {
 });
 
 function check(str) {
-  return (str === pi);
+  switch (selectElem.value) {
+    case 'one':
+      return (str === piOne);
+      break;
+    case 'two':
+      return (str === piTwo);
+      break;
+    default:
+      break;
+  }
 }
